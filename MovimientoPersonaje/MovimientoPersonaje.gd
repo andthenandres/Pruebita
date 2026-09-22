429f5b1e36b07848b456db5ce5e267fb39992ed4
Andres <andthenandres@gmail.com>
2026-09-22 15:04:32 +0200

    Commit animaciones actualizadas

--- MovimientoPersonaje/MovimientoPersonaje.gd
+++ MovimientoPersonaje/MovimientoPersonaje.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 200
+const SPRINTING = 400
 const JUMPSPEDD = -650
 @onready var personajeMain = $AnimatedSprite2D
 
@@ -10,69 +11,71 @@ func _ready():
 	personajeMain.sprite_frames.set_animation_on_loop_mode("dying", SpriteFrames.LoopMode.LOOP_NONE)
 
 func _physics_process(delta:float) ->void:
-	
+	move_and_slide()
 	#Movimiento
 	#Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	#Mov Horizontal
+	#MOVIMIENTO H 
 	if Input.is_action_pressed("Izq"):
-		velocity.x -= SPEED
-		personajeMain.play("walking")
-		
+		velocity.x = -SPEED
+	
+		if Input.is_action_pressed("Mayus"):
+			velocity.x = -SPRINTING
+
 	elif Input.is_action_pressed("Der"):
-		velocity.x += SPEED
-		personajeMain.play("walking")
+		velocity.x = SPEED
+	
+		if Input.is_action_pressed("Mayus"):
+			velocity.x = SPRINTING
+
 	else:
 		velocity.x = 0
-		personajeMain.play("idle")
-	
+
 	#Salto
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y += JUMPSPEDD
-		personajeMain.play("jumping")
 	
-	#Sprint
-	if(Input.is.action_pressed(""))
+	
 	
 	#RockFall
 	if not is_on_floor() and Input.is_action_pressed("Shift"):
 		velocity += (get_gravity() * delta) * 2
 		velocity.x = 0
-		if(is_on_floor and Input.is_action_just_released("Shift")):
+		if(is_on_floor() and Input.is_action_just_released("Shift")):
 			velocity += get_gravity() * delta	
-	#Animaciones 
 	
-	#Que el sprite tome la direccion al lado que se mueva 
-	if(direction < 0):
-		personaje.flip_h = false
-	elif(direction > 0):
-		personaje.flip_h = true
-	move_and_slide()
 	
-	func on_area_muerte_entered(body: Node2D) -> void:
-		
-		print = body.name
+	
+	
+func on_area_muerte_entered(body: Node2D) -> void:
 		dentro_del_area = true
 		
-	
+	#Aninmaciones
 func _process(delta):
-	ready
-			
-
-			
+	#Andar izq y der
+	if(velocity.x > 0):
+		personajeMain.play("walking")
+		personajeMain.flip_h = false
+	elif(velocity.x < 0):		
+		personajeMain.play("walking")
+		personajeMain.flip_h = true
+	#Correr horizontalmente
+	if(velocity.x > 200):
+		personajeMain.play("sprinting")
+		personajeMain.flip_h = false
+	elif(velocity.x < -200):
+		personajeMain.play("sprinting")
+		personajeMain.flip_h = true
+	#Saltar
+	if not is_on_floor():
+		personajeMain.play("jumping")
+	#Idle
+	if(velocity == Vector2(0,0)):
+		personajeMain.play("idle")
+	#Dying
+	if(dentro_del_area):
+		personajeMain.play("dying")
 		
 	
-		
 	
-		
-	
-	
-
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if personajeMain.animation == "die":
-		print("La ha palmao")
-		get_tree().quit()
-			void quit(exit_code: int = 0)
